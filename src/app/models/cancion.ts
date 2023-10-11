0b145d5d6f4e16aef98166b07f49d1c048b0c6b0
Santicm23 <santicastrom@outlook.com>
2023-10-11 16:08:31 -0500

    cuadrando tablas

--- src/app/models/cancion.ts
+++ src/app/models/cancion.ts
@@ -1,13 +1,14 @@
+import { Time } from '@angular/common'
+
 export class Cancion {
   constructor(
     public id: number,
     public nombre: string,
     public artista: string,
     public album: string,
-    public duracion: number,
+    public duracion: Time,
     public genero: string,
     public fecha: Date,
-    public imagen: string,
-    public usuario: number
-  ) { }
+    public imagen?: string
+  ) {}
 }
